08c0d3d1a88143c053faa46ce82533146573d0e8
vladeecc <vladeecc123@icloud.com>
2026-06-16 19:37:03 +0300

    версия библиотеки 1.0.0

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -9,12 +9,13 @@ public class Main {
     static void main() {
 
         Circle circle = new Circle(3);
-        Rectangle rectangle = new Rectangle(7, 12);
+        Rectangle rectangle = new Rectangle(8, 12);
         Triangle triangle = new Triangle(7, 7, 7);
 
         System.out.println("Круг");
         System.out.println("Площадь: " + circle.getArea());
         System.out.println("Периметр: " + circle.getPerimeter());
+        System.out.println("Диаметр: " + circle.getDiameter);
 
         System.out.println();
 
